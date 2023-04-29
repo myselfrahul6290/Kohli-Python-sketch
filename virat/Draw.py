613c06a6fa7ae3d1ffc167999be45e97e24e4c93
Rahul Shaw <103486958+myselfrahul6290@users.noreply.github.com>
2023-04-29 10:22:38 +0530

    Add files via upload

---
+++ virat/Draw.py
@@ -0,0 +1,39 @@
+from sketchpy import canvas
+
+
+# Create an object of canvas.sketch class
+Turtle = canvas.sketch(x_offset=650, y_offset=320)
+
+Turtle.draw_fn("Face", co=(255, 190, 160), mode=0)
+Turtle.draw_fn("Tshirt", co=(177, 178, 163), mode=0)
+
+
+Turtle.draw_fn("Neck", co=(255, 190, 160), mode=0)
+Turtle.draw_fn("NeckShadow", co=(191, 135, 112), mode=0)
+Turtle.draw_fn("EarShade", co=(193, 135, 113), mode=0)
+Turtle.draw_fn("Beard1", co=(46, 3, 23), mode=0)
+
+Turtle.draw_fn("Ear", co=(255, 190, 160), mode=0)
+Turtle.draw_fn("EarShade1", co=(0, 0, 0), mode=0)
+Turtle.draw_fn("liparea", co=(255, 190, 160), mode=0)
+Turtle.draw_fn("lipareas", co=(188, 78, 82), mode=0)
+Turtle.draw_fn("lipsShade", co=(113, 31, 33), mode=0)
+
+Turtle.draw_fn("LeftPartShade", co=(232, 166, 135), mode=0)
+
+Turtle.draw_fn("EaybrowL", co=(46, 3, 23), mode=0)
+Turtle.draw_fn("EyebrowR", co=(46, 3, 23), mode=0)
+
+Turtle.draw_fn("EayL", co=(211, 210, 210), mode=0)
+
+Turtle.draw_fn("EyeR", co=(211, 210, 210), mode=0)
+Turtle.draw_fn("NoseLine", co=(0, 0, 0), mode=0)
+Turtle.draw_fn("Nosehole1", co=(0, 0, 0), mode=0)
+Turtle.draw_fn("Nosehole2", co=(0, 0, 0), mode=0)
+
+
+Turtle.draw_fn("eayBallL", co=(30, 1, 14), mode=0)
+Turtle.draw_fn("eyeBallR", co=(30, 1, 14), mode=0)
+Turtle.draw_fn("HairShade",retain=True, co=(168, 116, 95), mode=0)
+
+
